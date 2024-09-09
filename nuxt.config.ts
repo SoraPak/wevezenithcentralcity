eec9713fa86e90f0c1b0db13109dd6f440a8ee67
delio-victim <delio.victim@gmail.com>
2024-09-09 20:35:59 +0900

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
     prerender: {
-      routes: ['/'] // 특정 경로만 사전 렌더링
+      routes: ['/'] // 필요 없는 메타 파일을 생성하지 않도록 특정 경로만 지정
     }
   },
+  router: {
+    base: '/'
+  },
   target: 'static', // 정적 사이트 빌드용 설정
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: [
+    '~/assets/css/reset.css',
+    '~/assets/css/common.css',
+  ],
+  build: {
+    filenames: {
+      app: ({ isDev }) => isDev ? '[name].js' : '[name].[contenthash].js',
+      chunk: ({ isDev }) => isDev ? '[name].js' : '[name].[contenthash].js',
+      css: ({ isDev }) => isDev ? '[name].css' : '[name].[contenthash].css',
+      img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[name].[contenthash:7].[ext]',
+      font: ({ isDev }) => isDev ? '[path][name].[ext]' : 'fonts/[name].[contenthash:7].[ext]',
+      video: ({ isDev }) => isDev ? '[path][name].[ext]' : 'videos/[name].[contenthash:7].[ext]'
+    }
+  },
   generate: {
-    fallback: true,
-    dir: '.output/public' // 빌드 결과물을 저장할 디렉토리 명시
+    fallback: true // 404 페이지를 위해 필요
   },
   app: {
     baseURL: '/',
@@ -19,14 +38,17 @@ export default defineNuxtConfig({
         { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
         { property: 'og:type', content: 'website' },
         { name: 'keywords', content: '두산위브,제니스,학정역,분양,모델하우스,견본하우스' },
-        { name: 'description', content: '대구 랜드마크의 자격 오직, 제니스!' },
+        { name: 'description', content: '대구 랜드마크의 자격 오직, 제니스! 대구 또 하나의 랜드마크, 새로운 제니스가 옵니다. 학정역, 칠곡경대병원역 더블 역세권, 중도금 무이자, 전매 무제한, 계약금500만원! 모델하우스에서 박소라 과정을 찾아주시면 푸짐한 사은품 증정!!' },
         { property: 'og:site_name', content: '학정역 두산위브더제니스 센트럴시티' },
         { property: 'og:title', content: '학정역 두산위브더제니스 센트럴시티' },
-        { property: 'og:description', content: '대구 랜드마크의 자격 오직, 제니스!' },
+        { property: 'og:description', content: '대구 랜드마크의 자격 오직, 제니스! 대구 또 하나의 랜드마크, 새로운 제니스가 옵니다. 학정역, 칠곡경대병원역 더블 역세권, 중도금 무이자, 전매 무제한, 계약금500만원! 모델하우스에서 박소라 과정을 찾아주시면 푸짐한 사은품 증정!!' },
         { property: 'og:image', content: '/og_img.png' },
+        { property: 'og:image:type', content: 'image/png' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '622' },
         { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:title', content: '학정역 두산위브더제니스 센트럴시티' },
-        { name: 'twitter:description', content: '대구 랜드마크의 자격 오직, 제니스!' },
+        { name: 'twitter:description', content: '대구 랜드마크의 자격 오직, 제니스! 대구 또 하나의 랜드마크, 새로운 제니스가 옵니다. 학정역, 칠곡경대병원역 더블 역세권, 중도금 무이자, 전매 무제한, 계약금500만원! 모델하우스에서 박소라 과정을 찾아주시면 푸짐한 사은품 증정!!' },
         { name: 'twitter:image', content: '/og_img.png' },
       ],
       link: [
@@ -36,7 +58,7 @@ export default defineNuxtConfig({
         { rel: 'shortcut icon', href: '/icon.svg' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat&display=swap' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap' },
       ]
     }
   }
