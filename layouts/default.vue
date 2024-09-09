27f741ec96584dcf977245456317ee4b647def90
SoraPak <soragee86@gmail.com>
2024-09-09 18:06:15 +0900

    header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bodWrap">
     <header>
-      <h1>두산위브더제니스 센트럴시티</h1>
+      <h1 class="logo"><nuxt-link to="/"><img src="/public/images/logo.svg" alt="학정역 두산위브더제니스 센트럴시티"></nuxt-link></h1>
       <nav>
         <ul>
           <li><nuxt-link to="/about">사업개요</nuxt-link></li>
@@ -10,7 +10,7 @@
           <li><nuxt-link to="/space">세대안내</nuxt-link></li>
         </ul>
       </nav>
-      <div><span>010</span>9357.0669</div>
+      <div class="tel"><div><span>010</span>9357.0669</div></div>
     </header>
     <main>
       <NuxtPage />
@@ -27,7 +27,68 @@
   flex-direction: column;
   height: 100%;
 }
+header{
+  display:flex;
+  justify-content: center;
+  
+  min-height: 80px;
+  padding: 0 10px;
+  border-bottom: 5px solid #084290;
+}
 main {
   flex: 1;
+}
+.logo {
+  display: flex;
+  margin-right: auto;
+}
+.logo a{
+  display: flex;
+  align-items: center;
+}
+.logo img{
+  width: 200px;
+  height: auto;
+
+}
+.tel {
+  margin-left: auto;
+  display: flex;
+  justify-content: center;
+}
+nav {
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  text-align: center;
+  margin: 0 10px;
+  max-width: 500px;
+}
+nav a,
+nav a:hover {
+  color: #3b3838;
+}
+nav ul {
+  display: flex;
+  flex: 1;
+}
+nav ul li {
+  display: flex;
+  flex: 1;
+  text-align: center;
+}
+nav ul li a{
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+}
+nav ul li a::before{
+  content: "";
+  display: block;
+  width: 54px;
+  height: 54px;
+  background: #e06f6f;
 }
 </style>
