89ec1e0dbf61f67e56d538887b1f7afc9f6eb57a
SoraPak <soragee86@gmail.com>
2024-09-09 18:21:40 +0900

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ['/'] // 필요 없는 메타 파일을 생성하지 않도록 특정 경로만 지정
+    }
+  },
   router: {
     base: '/'
   },
