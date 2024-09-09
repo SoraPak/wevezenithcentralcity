78326e87df7ae18297728ff020466850a7a706ff
SoraPak <soragee86@gmail.com>
2024-09-09 14:07:25 +0900

    setup layaout

---
+++ components/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <img src="/images/header.jpg" alt="헤더 이미지" />
+    <nav>
+      <!-- 네비게이션 메뉴 -->
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: #f8f9fa;
+}
+
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
