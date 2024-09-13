b13c99b4a3531a5dcdae293f6b25ab83b5a6b495
SoraPak <soragee86@gmail.com>
2024-09-13 16:31:40 +0900

    update hero

--- components/landing/hero.vue
+++ components/landing/hero.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="hero">
-    <!-- h1 class="logo"><img src="/images/full_logo_w.svg" width="244" alt="두산위브더제니스 센트럴시티"></h1 -->
+    <!-- h1 class="logo"><img src="/images/full_logo_w.svg" width="160" alt="두산위브더제니스 센트럴시티"></h1 -->
     <h2 class="ttl">
-      <img src="/images/landing/hero/ttl01.png" width="607" alt="대구 랜드마크의 자격">
-      <img src="/images/landing/hero/ttl02.png" width="797" alt="오직, 제니스">
-      <img src="/images/landing/hero/ttl03.png" width="830" alt="대구 또 하나의 랜드마크, 새로운 제니스가 옵니다">
+      <img src="/images/landing/hero/ttl01.png" width="425" alt="대구 랜드마크의 자격">
+      <img src="/images/landing/hero/ttl02.png" width="558" alt="오직, 제니스">
+      <img src="/images/landing/hero/ttl03.png" width="581" alt="대구 또 하나의 랜드마크, 새로운 제니스가 옵니다">
     </h2>
     <div class="textBox1">
-      <p class="text1"><img src="/images/simple_logo_w.svg" width="566" alt="두산위브더제니스 센트럴시티"></p>
-      <p class="text2"><img src="/images/landing/hero/text02.png" width="565" alt="전용 84・115・143・191㎡ | 총 1,098세대"></p>
+      <p class="text1"><img src="/images/simple_logo_w.svg" width="400" alt="두산위브더제니스 센트럴시티"></p>
+      <p class="text2"><img src="/images/landing/hero/text02.png" width="395" alt="전용 84・115・143・191㎡ | 총 1,098세대"></p>
     </div>
   </div>
 </template>
 <style scoped>
 .hero {
   background: url("/images/landing/hero/bg_main.png") no-repeat center bottom #001135;
-  background-size: 1920px auto;
+  background-size: 1344px auto;
   /* height: 1500px; */
-  height: 1170px;
+  height: 840px;
   color: #ffffff;
   position: relative;
 }
@@ -31,10 +31,8 @@
   position: absolute;
   left: 50%;
   transform: translate(-50%, 0);
-  width: 850px;
-  height: 315px;
-  /* bottom:998px; */
-  bottom:738px;
+  height: 220px;
+  top:80px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -51,6 +49,6 @@
   left: 50%;
   transform: translate(-50%, 0);
   /* bottom:806px; */
-  bottom:548px;
+  top:335px;
 }
 </style>
