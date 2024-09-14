3ce76fa1dd2e7969a0f0aee717911e4df66821c5
delio-victim <delio.victim@gmail.com>
2024-09-14 13:05:10 +0900

    update

--- components/landing/ansim.vue
+++ components/landing/ansim.vue
@@ -1,13 +1,14 @@
 <template>
   <section class="ansim">
     <div class="ansim_inner">
+      <div class="bg"><span></span><span></span><span></span></div>
       <h1 class="ttl"><img src="/images/landing/ansim/ttl.png" width="571" alt="할인 분양 걱정 NO! 후계약자와 동일한 혜택 보장!"></h1>
       <p class="text01"><img src="/images/landing/ansim/text01.png" width="572" alt="지금 바로 프리미엄 동호수 선택하세요"></p>
       <img class="img01" src="/images/landing/ansim/img01.png" width="496" alt="">
       <dl class="section01">
         <dt><img src="/images/landing/ansim/sub_ttl.png" width="444" alt="계약조건 안심 보장제 실시!"></dt>
-        <dd>분양정책 등의 변경으로 최초 공급계약체결 당시의
-        조건 대비 유리하게 변경될경우, 기존계약자에 대하여
+        <dd>분양정책 등의 변경으로 최초 공급계약체결 당시의<br>
+        조건 대비 유리하게 변경될경우, 기존계약자에 대하여<br>
         변경된 계약조건으로 소급하여 혜택을 부여하는제도 
         </dd>
       </dl>
@@ -26,22 +27,63 @@
     margin: 0 auto;
     position: relative;
     height: 740px;
-    background: #ffffff31;
+    padding-top: 210px;
   }
+  .bg {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+  }
+  .bg span:nth-child(1),
+  .bg span:nth-child(2),
+  .bg span:nth-child(3) {
+    display: block;
+    position: absolute;
+  }
+  .bg span:nth-child(1) {
+    top: -210px;
+    right: -490px;
+    width: 577px;
+    height: 518px;
+    background: url("/images/landing/ansim/bg01.png") no-repeat center center;
+    background-size: 100% auto;
+  }
+
+  .bg span:nth-child(2) {
+    bottom: 0;
+    left: -420px;
+    width: 594px;
+    height: 594px;
+    background: url("/images/landing/ansim/bg02.png") no-repeat center center;
+    background-size: 100% auto;
+  }
+
+  .bg span:nth-child(3) {
+    bottom: -20px;
+    right: -900px;
+    width: 872px;
+    height: 621px;
+    background: url("/images/landing/ansim/bg03.png") no-repeat center center;
+    background-size: 100% auto;
+  }
+
+
   .ttl, .text01, .img01 {
     position: absolute;
     z-index: 2;
   }
   .ttl {
-    top: 0;
+    top: -320px;
   }
   .img01 {
     right: -81px;
-    top: -60px;
+    top: -387px;
   }
   .text01 {
     left: -57px;
-    top: 155px;
+    top: -155px;
   }
   .section01 {
     width: 672px;
@@ -49,5 +91,10 @@
     color: #606563;
     background: #fff;
     padding: 33px;
+    border-radius: 30px;
+    text-align: center;
+    font-size: 25px;
+    line-height: 160%;
+    position: relative;
   }
 </style>
