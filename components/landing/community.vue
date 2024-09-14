af8e151485d061271be9b5c2b14867fa0f1300ad
delio-victim <delio.victim@gmail.com>
2024-09-15 02:22:31 +0900

    creat reveiw section

--- components/landing/community.vue
+++ components/landing/community.vue
@@ -1,10 +1,54 @@
 <template>
-  <div>
-    <h1>홈페이지 개발중</h1>
-    <p>This is the homepage of the website.afdsafds</p>
-  </div>
+  <section class="community">
+
+      <h1 class="ttl"><img src="/images/landing/community/ttl.png" width="584" alt="입주민을 위한 고품격 커뮤니티 CLUB ZENITH"></h1>
+      <img class="img01" src="/images/landing/community/img.jpg" width="885" alt="">
+      <ol class="imgList">
+        <li><img src="/images/landing/community/img01.jpg" width="371" alt="">피트니스 센터, 웨이트 존, GX룸</li>
+        <li><img src="/images/landing/community/img02.jpg" width="371" alt="">자녀들을 위한 키즈카페와 작은 도서관</li>
+        <li><img src="/images/landing/community/img03.jpg" width="371" alt="">카페테리아와 휴게 라운지</li>
+        <li><img src="/images/landing/community/img04.jpg" width="371" alt="">칠곡 최초의 온탕, 냉탕 사우나</li>
+        <li><img src="/images/landing/community/img05.jpg" width="371" alt="">실내 골프 연습장 및 스크린 골프</li>
+      </ol>
+
+
+  </section>
 </template>
 
-<script setup>
-
-</script>
+<style scoped>
+  .community {
+    position: relative;
+    height: 1360px;
+    color: #000f3a;
+  }
+  .community::before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: url("/images/landing/community/bg_bottom.png") no-repeat center bottom;
+    background-size: 2688px auto;
+  }
+  .ttl, .img01, .imgList {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, 0);
+  }
+  .ttl {
+    top: -20px;
+  }
+  .img01 {
+    top: 155px;
+  }
+  .imgList {
+    top: 885px;
+    display: flex;
+    justify-content: center;
+  }
+  .imgList li{
+    margin: 0 13px;
+  }
+</style>
--- components/landing/reviews.vue
+++ components/landing/reviews.vue
@@ -1,10 +1,254 @@
 <template>
-  <div>
-    <h1>홈페이지 개발중... 감사합니다</h1>
-    <p>This is the homepage of the website.afdsafds</p>
-  </div>
+  <section class="reviews">
+      <h1 class="ttl">
+        <span>지금 바로 두산위브더제니스 센트럴시티와 함께</span>
+        <span>프리미엄 라이프를 준비하세요!</span>
+      </h1>
+      <div class="reviewCon">
+        <div>
+          <dl>
+            <dt>의료 접근성 완전 좋아요!</dt>
+            <dd>
+              <p>
+                나이 드신 부모님을 모시고 살고 있습니다.
+                부모님 건강이 제일 중요한데, 두산위브더
+                제니스 센트럴시티는 칠곡경북대학교병원이
+                바로 옆에 있어서 정말 안심이 되더라고요.
+              </p>
+              <p>
+                저희 부모님은 나이가 드셔서 정기적으로
+                병원을 다니셔야 하는데, 여기 살면 걸어서도
+                대학 병원에 갈 수 있을 정도로 가까워서 너무
+                편리할 것 같아요. 칠곡경북대학교병원은 대형
+                의료기관이라, 부모님뿐만 아니라 온 가족이
+                믿고 다닐 수 있겠다는 생각이 들었어요.
+              </p>
+              <p>
+                이 아파트라면 부모님이 갑자기 몸이 불편하실
+                때도 병원에 금방 갈 수 있어서 걱정 없이 살 수
+                있을 것 같아요. 
+              </p>
+              <p>
+                태전동 58세 김**
+              </p>
+              <img src="/images/landing/reviews/img01.png" width="166" alt="">
+            </dd>
+          </dl>
+
+          <dl>
+            <dt>역세권 새 아파트로 갈아타기 딱!</dt>
+            <dd>
+              <p>
+                대구 칠곡에서 30년 정도 살고 있는 **이편한 주민이에요. 지금 아파트도 너무 만족하면서
+                살고 있긴 한데 팔거역까지 걸어서 15분 정도
+                걸리는 게 좀 불편하더라고요.
+                그래서 항상 자가용을 끌고 다니는데,
+                대구 시내 주차가 어려워서 시내에는 잘 안 가게
+                되네요.
+              </p>
+              <p>
+                역세권에 살면 매천시장에도 자주 갈 수 있을
+                텐데, 주차 문제 때문에 발길이 잘 안 가더
+                라고요. 또 지금 살고 있는 아파트도 15년
+                정도로 좀 낡아서 새 아파트로 갈아탈까 고민
+                중이었거든요. 그런데 두산위브더제니스
+                센트럴시티는 학정역 바로 옆에 있는 역세권
+                아파트고, 고급 새 아파트라 관심이 확 생기더
+                라고요!
+              </p>
+              <p>
+                역세권에 새 아파트까지 완벽한 조건이니,
+                고민이 점점 기울고 있어요.
+              </p>
+              <p>
+                읍내동 62세 함**
+              </p>
+              <img src="/images/landing/reviews/img03.png" width="166" alt="">
+            </dd>
+          </dl>
+
+          <dl>
+            <dt>안심보장제로 걱정 없이 믿고 계약!!</dt>
+            <dd>
+              <p>
+                모델하우스 딱 들어갔을 때 깔끔한 분위기랑
+                세련된 외관이 눈에 확 들어오더라고요.
+                사전 예약하고 갔더니 신현주 팀장님이 친절
+                하게 맞아주셨어요.
+              </p>
+              <p>
+                요즘 미계약 물량으로 할인 분양하는 곳이
+                많아 일찍 계약하면 손해일까 걱정됐는데,
+                안심보장제 덕분에 걱정이 싹 사라짐!
+                후계약자와 동일한 조건으로 혜택이 보장
+                된다고 하니 믿고 계약했습니다.
+              </p>
+              <p>
+                구암동 30세 박**
+              </p>
+              <img src="/images/landing/reviews/img05.png" width="166" alt="">
+            </dd>
+          </dl>
+        </div>
+        
+        <div>
+          <dl>
+            <dt>아이 키우기 좋은 아파트,<br>학교 걱정도 없어요!</dt>
+            <dd>
+              <p>
+                초등학생 아이를 키우는 엄마 입장에서
+                모델하우스를 다녀왔는데, 솔직히 단점도
+                있지만 장점이 너무 커서 고민이 해결됐어요!
+                주변에 초등학교가 가까이 없다는 게 조금
+                아쉽긴 했지만, 스쿨버스가 제공된다고 해서
+                안심됐어요. 아이가 안전하게 학교를 오갈 수
+                있는 점은 엄마들한테 정말 중요한 부분이잖
+                아요?
+              </p>
+              <p>
+                그리고 단지 자체가 지상에 차가 없는 설계라서
+                아이들이 아파트 내에서 안전하게 뛰어놀 수
+                있어요. 요즘 이런 안전한 환경은 정말 큰 장점
+                인것 같아요. 더불어 팔거천 근린공원이 바로
+                옆에 있어서 아이랑 산책도 하고 자연 속에서
+                마음껏 뛰어놀 수 있겠더라고요!
+                주말엔 가족끼리 운암지 서리지에서 자연을
+                느끼며 힐링할 생각하니 벌써 기대돼요.
+              </p>
+              <p>
+                아이 키우기 좋은 안전하고 자연친화적인
+                아파트를 찾으시는 분들께
+                꼭 추천하고 싶어요! 😊
+              </p>
+              <p>
+                도남동 43세 윤**
+              </p>
+              <img src="/images/landing/reviews/img02.png" width="166" alt="">
+            </dd>
+          </dl>
+          <dl>
+            <dt>운동도 사우나도 한 번에,<br>이건 진짜 딱이네!</dt>
+            <dd>
+              <p>
+                저는 운동을 정말 좋아하는데요, 살고 있는
+                아파트에도 골프장이랑 헬스클럽 같은 운동
+                커뮤니티 시설이 있긴 한데, 사우나가 없어서
+                자주 이용하게 되진 않더라고요.
+                운동 후에 땀 흘리고 나면 사우나에서
+                시원하게 씻는 게 필수인데, 그게 안 되니깐
+                아무래도 이용 빈도가 떨어지더라고요.
+              </p>
+              <p>
+                그런데 두산위브더제니스 센트럴시티는 제가
+                딱 원하는 조건을 다 갖추고 있어요! 골프장도
+                넓고 쾌적하게 되어있고, 무엇보다 사우나
+                시설이 너무 잘 돼 있는것같더라고요.
+                사우나가 있어서 운동 후에도 바로 씻고
+                개운하게 하루를 마무리할 수 있으니 정말
+                자주 이용하게 될 것 같아요.
+              </p>
+              <p>
+                운동을 좋아하는 사람들에게는 이런 시설이
+                얼마나 중요한지 아실 거예요. 여기라면
+                골프도 치고, 헬스도 하고, 사우나까지 즐기
+                면서 건강하고 활기찬 생활을 이어갈 수 있을
+                것 같습니다.
+              </p>
+              <p>
+                동천동 35세 최**
+              </p>
+              <img src="/images/landing/reviews/img04.png" width="166" alt="">
+            </dd>
+          </dl>
+        </div>
+      </div>
+  </section>
 </template>
 
-<script setup>
+<style scoped>
+.reviews {
+  background: #be9167;
+  color: #fff;
+  padding-top: 30px;
+  padding-bottom: 255px;
+}
+.ttl {
+  width: 780px;
+  text-align: center;
+  border-top: 3px solid #fff;
+  border-bottom: 3px solid #fff;
+  position: relative;
+  margin: 0 auto;
+  padding: 30px 0;
+}
+.ttl::before {
+  content: "";
+  display: block;
+  width: 426px;
+  height: 101px;
+  background: url("/images/landing/reviews/ttl_deco.svg") no-repeat center bottom;
+  background-size: 426px auto;
+  position: absolute;
+  top: -106px;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+.ttl span:first-child {
+  font-size: 41px;
+}
+.ttl span:last-child {
+  font-size: 63px;
+  margin-top: 40px;
+}
+.ttl span {
+  display: block;
+}
+.reviewCon {
+  display: flex;
+  align-items: center;
+  min-height: 500px;
+  width: 840px;
+  margin: 42px auto 0;
+}
+.reviewCon div:first-child{
+  flex: 1;
+  margin-right: 15px;
+}
+.reviewCon div:last-child{
+  flex: 1;
+  margin-left: 15px;
+}
+.reviewCon dl{
+  background: #fff;
+  color: #000f3a;
+  padding: 33px;
+  border-radius: 25px;
+  margin-top: 115px;
+  font-size: 19px;
+  line-height: 160%;
+  word-break: keep-all;
+  position: relative;
+}
+.reviewCon dl:first-child{
+  margin-top: 0;
+}
+.reviewCon dl img {
+  position: absolute;
+  right: 40px;
+  bottom: -90px;
+}
 
-</script>
+.reviewCon p {
+  margin-top: 20px;
+}
+.reviewCon dt {
+  font-weight: 800;
+  font-size: 23px;
+  line-height: 140%;
+}
+.reviewCon dd p:last-child {
+  font-weight: 600;
+}
+
+
+</style>
