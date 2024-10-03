4956c75ac673290f8323dadd98df5de67dd6624c
delio-victim <delio.victim@gmail.com>
2024-10-04 08:52:23 +0900

    update

--- components/landing/r_nav.vue
+++ components/landing/r_nav.vue
@@ -8,6 +8,7 @@
     </div>
     <div class="section03">
       <a class="kakaoBut" href="https://open.kakao.com/me/wevehj" target="_blank">
+        
         <span><img loading="lazy" src="/images/landing/r_nav/kakao.svg" width="45" alt=""></span>
         톡상담/예약
       </a>
