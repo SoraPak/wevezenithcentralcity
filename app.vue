7d8515e09d8563a9c9e9ea6a2b118bd1a96bc53f
SoraPak <soragee86@gmail.com>
2024-10-28 13:21:58 +0900

    update

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+import { useHead } from "#app";
+
+useHead({
+  meta: [
+    { property: 'og:image', content: 'https://xn--hu1bq8p65bh1h6oby7bg90c.kr/og_img_sora.png', id: 'dynamic-og-image' }
+  ]
+});
+</script>
---
+++ plugins/dynamic-og-image.client.ts
@@ -0,0 +1,26 @@
+// plugins/dynamic-og-image.client.ts
+export default defineNuxtPlugin(() => {
+  if (process.client) {
+    function updateOgImage() {
+      const hash = window.location.hash;
+      const ogImageMeta = document.getElementById('dynamic-og-image');
+
+      // 기본 OG 이미지
+      let newImage = "https://xn--hu1bq8p65bh1h6oby7bg90c.kr/og_img_sora.png";
+
+      // 특정 해시 값에 따라 OG 이미지 변경
+      if (hash === "#study") {
+        newImage = "https://xn--hu1bq8p65bh1h6oby7bg90c.kr/og_img_study.png";
+      }
+
+      // meta 태그 업데이트
+      if (ogImageMeta) {
+        ogImageMeta.setAttribute("content", newImage);
+      }
+    }
+
+    // 페이지 로드 시와 해시 변경 시 업데이트
+    window.addEventListener('load', updateOgImage);
+    window.addEventListener('hashchange', updateOgImage);
+  }
+});
