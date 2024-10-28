15f4b4a0c0a819cba6451e0ae7fd170c9202cb0d
SoraPak <soragee86@gmail.com>
2024-10-28 13:44:26 +0900

    update

--- app.vue
+++ app.vue
@@ -3,13 +3,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<script setup>
-import { useHead } from "#app";
-
-useHead({
-  meta: [
-    { property: 'og:image', content: 'https://학정역두산위브.kr/og_img_sora.png', id: 'dynamic-og-image' }
-  ]
-});
-</script>
