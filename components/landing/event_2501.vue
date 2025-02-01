a3e8f90d8052ce88362b5728e998ee117c60ab88
delio-victim <delio.victim@gmail.com>
2025-02-01 09:52:41 +0900

    up

--- components/landing/event_2501.vue
+++ components/landing/event_2501.vue
@@ -11,13 +11,63 @@
       <p class="sec01_text01"><img loading="lazy" src="/images/landing/event(2501)/sec01_text01.png" width="418" alt="박소라 과장 찾아오시면 행운의 경품 복권을 드립니다!" /></p>
       <p class="sec01_text02"><img loading="lazy" src="/images/landing/event(2501)/sec01_text02.png" width="298" alt="배포 장소: 두산위브더제니스 센트럴시티 모델하우스 | 배포 조건: 박소라 과장 찾아오셔서 팀장 상담 완료 시(예약필수)" /></p>
       <div class="ps01_01"></div>
+      <div class="ps01_02"></div>
+      <div class="ps01G">
+        <div class="ps01_03">
+          <span>온누리 상품권</span>
+        </div>
+        <div class="ps01_04">
+          <span>각티슈 세트</span>
+        </div>
+        <div class="ps01_05">
+          <span>공기 청정기</span>
+        </div>
+        <div class="ps01_06">
+          <span>라면세트</span>
+        </div>
+        <div class="ps01_07">
+          <span>리빙세트</span>
+        </div>
+        
+      </div>
+      <div class="ps01_08"></div>
+      <p class="ps01_remark">※이미지는 참고용으로 실제와 다를 수 있으며, 사정에 따라 상품이 변경될 수 있습니다.</p>
+      
+      <div class="sec02">
+        <p class="sec02_text01"><img loading="lazy" src="/images/landing/event(2501)/sec02_text01.png" width="403" alt="신년맞이 방문자 경품 행사" /></p>
+        <p class="sec02_text02"><img loading="lazy" src="/images/landing/event(2501)/sec02_text02.png" width="628" alt="2월 1일(토) 모델하우스 방문하시면 경품 추첨권을드려요!" /></p>
+        <p class="sec02_text03"><img loading="lazy" src="/images/landing/event(2501)/sec02_text03.png" width="471" alt="추첨 일시: 2025년 2월 1일(토) 오후 3시(10분 전 추첨권 배포) | 배포 조건: 박소라 과장 찾아오셔서 팀장 상담 완료 시(예약필수)" /></p>
+        <div class="ps02G">
+          <div class="ps02_01">
+            <span>삼성 TV</span>
+          </div>
+          <div class="ps02_02">
+            <span>다이슨 에어랩</span>
+          </div>
+          <div class="ps02_03">
+            <span>공기 청정기</span>
+          </div>
+          <div class="ps02_04">
+            <span>LG세탁건조기</span>
+          </div>
+          <div class="ps02_05">
+            <span>무선청소기</span>
+          </div>
+          <div class="ps02_06">
+            <span>다이슨<br> 헤어드라이기</span>
+          </div>
+          <div class="ps02_07">
+            <span>신세계 상품권</span>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <style scoped>
   .event2501 {
-    background: url("/images/landing/event(2501)/bg.jpg") no-repeat center top #000f3a;
+    background: url("/images/landing/event(2501)/bg.jpg") no-repeat center -70px #000f3a;
     background-size: 1500px auto;
     position: relative;
     padding-top: 130px;
@@ -85,11 +135,194 @@
   .sec01_text02 {
     text-align: center;
   }
+  [class^="ps01_"], [class^="ps02_"] {
+    position: absolute;
+  }
+  [class^="ps01_"] > span, [class^="ps02_"] > span {
+    position: absolute;
+    text-align: center;
+    color: #b91210;
+    font-weight: bolder;
+    font-size: 20px;
+    text-shadow: rgb(255, 255, 255) 2px 0px 0px, rgb(255, 255, 255) 1.75517px 0.958851px 0px, rgb(255, 255, 255) 1.0806px 1.68294px 0px, rgb(255, 255, 255) 0.141474px 1.99499px 0px, rgb(255, 255, 255) -0.832294px 1.81859px 0px, rgb(255, 255, 255) -1.60229px 1.19694px 0px, rgb(255, 255, 255) -1.97998px 0.28224px 0px, rgb(255, 255, 255) -1.87291px -0.701566px 0px, rgb(255, 255, 255) -1.30729px -1.5136px 0px, rgb(255, 255, 255) -0.421592px -1.95506px 0px, rgb(255, 255, 255) 0.567324px -1.91785px 0px, rgb(255, 255, 255) 1.41734px -1.41108px 0px, rgb(255, 255, 255) 1.92034px -0.558831px 0px;
+  }
   .ps01_01 {
     width: 271px;
     height: 244px;
     background: url("/images/landing/event(2501)/ps01_01.png") no-repeat center top;
     background-size: 100% auto;
+    left: 70px;
+    top: 536px;
+  }
+  .ps01_02 {
+    width: 290px;
+    height: 174px;
+    background: url("/images/landing/event(2501)/ps01_02.png") no-repeat center top;
+    background-size: 100% auto;
+    right: 0;
+    top: 580px;
+  }
+  .ps01G {
+    position: relative;
+    width: 100%;
+    margin-top: 20px;
+  }
+  .ps01_03 {
+    width: 242px;
+    height: 184px;
+    background: url("/images/landing/event(2501)/ps01_03.png") no-repeat center top;
+    background-size: 100% auto;
+    left: -70px;
+    top: 80px;
+  }
+  .ps01_03 > span {
+    bottom: 30px;
+    left: -10px;
+  }
+  .ps01_04 {
+    width: 280px;
+    height: 159px;
+    background: url("/images/landing/event(2501)/ps01_04.png") no-repeat center top;
+    background-size: 100% auto;
+    left: 130px;
+    top: 130px;
+    z-index: 1;
+  }
+  .ps01_04 > span {
+    width: 100%;
+    bottom: 30px;
+    left: -10px;
+  }
+  .ps01_05 {
+    width: 188px;
+    height: 262px;
+    background: url("/images/landing/event(2501)/ps01_05.png") no-repeat center top;
+    background-size: 100% auto;
+    left: 352px;
+    top: -13px;
+  }
+  .ps01_05 > span {
+    width: 100%;
+    bottom: -10px;
+    left: 0;
+  }
+  .ps01_06 {
+    width: 221px;
+    height: 177px;
+    background: url("/images/landing/event(2501)/ps01_06.png") no-repeat center top;
+    background-size: 100% auto;
+    right: 120px;
+    top: 140px;
+    z-index: 1;
+  }
+  .ps01_06 > span {
+    width: 100%;
+    left: 0;
+    bottom: 31px;
+  }
+  .ps01_07 {
+    width: 260px;
+    height: 175px;
+    background: url("/images/landing/event(2501)/ps01_07.png") no-repeat center top;
+    background-size: 100% auto;
+    right: -97px;
+    top: 114px;
+  }
+  .ps01_07 > span {
+    width: 100%;
+    left: -40px;
+    bottom: 31px;
+  }
+  .ps01_08 {
+    width: 150px;
+    height: 109px;
+    background: url("/images/landing/event(2501)/ps01_08.png") no-repeat center top;
+    background-size: 100% auto;
+    top: 472px;
+    left: 105px;
+  }
+  .ps01_remark {
+    font-size: 15px;
+    text-align: center;
+    width: 100%;
+    margin-top: 276px;
+    position: relative;
+  }
+
+  .sec02 {
+    margin-top: 50px;
+    text-align: center;
+  }
+  .ps02G {
+    position: relative;
+    background: #fff;
+    min-height: 20px;
+  }
+  .ps02_01 {
+    width: 328px;
+    height: 288px;
+    background: url("/images/landing/event(2501)/ps02_01.png") no-repeat center top;
+    background-size: 100% auto;
+    left: -40px;
+    top: 100px;
+  }
+  .ps02_02 {
+    width: 154px;
+    height: 255px;
+    background: url("/images/landing/event(2501)/ps02_02.png") no-repeat center top;
+    background-size: 100% auto;
+    left: 180px;
+    top: 40px;
+    z-index: 1;
+  }
+  .ps02_02 > span {
+    width: 120%;
+  }
+  .ps02_03 {
+    width: 205px;
+    height: 260px;
+    background: url("/images/landing/event(2501)/ps02_03.png") no-repeat center top;
+    background-size: 100% auto;
+    left: 222px;
+    top: 140px;
+  }
+  .ps02_03 > span {
+    width: 100%;
+    left: 0;
+    bottom: 12px;
+  }
+  .ps02_04 {
+    width: 338px;
+    height: 409px;
+    background: url("/images/landing/event(2501)/ps02_04.png") no-repeat center top;
+    background-size: 100% auto;
+    left: 340px;
+    top: 10px;
+  }
+  .ps02_05 {
+    width: 268px;
+    height: 411px;
+    background: url("/images/landing/event(2501)/ps02_05.png") no-repeat center top;
+    background-size: 100% auto;
+    right: 120px;
+    top: 0;
+  }
+  .ps02_06 {
+    width: 160px;
+    height: 253px;
+    background: url("/images/landing/event(2501)/ps02_06.png") no-repeat center top;
+    background-size: 100% auto;
+    right: 70px;
+    top: 160px;
+    z-index: 1;
+  }
+  .ps02_07 {
+    width: 143px;
+    height: 234px;
+    background: url("/images/landing/event(2501)/ps02_07.png") no-repeat center top;
+    background-size: 100% auto;
+    right: -10px;
+    top: 142px;
   }
 
 
