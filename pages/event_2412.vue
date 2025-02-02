d8ff46d48049284347ff45c52b6f5796c7e131bb
SoraPak <soragee86@gmail.com>
2025-02-02 17:12:03 +0900

    up

---
+++ pages/event_2412.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="event_list">
+    <p class="comment">종료된 이벤트입니다</p>
+    <Xmas />
+    <Event_nav />
+  </div>
+</template>
+
+<script setup>
+import { useHead } from '#imports';
+import Xmas from '~/components/landing/x-mas.vue';
+import Event_nav from '~/components/event_nav.vue';
+
+useHead({
+  style: [
+    {
+      children: `
+        .gift {
+          padding-top: 70px !important;
+        }
+        .gift::after {
+          display: none !important;
+        }
+      
+        @media only screen and (max-width: 950px) {
+          .gift {
+            padding-top: 29vw !important;
+            margin-bottom: 0 !important;
+          }
+          .gift::before {
+            height: 39vw;
+            bottom: -6vw;
+          }
+          .gift_inner {
+            padding-bottom: 26vw;
+          }
+        }
+      `
+    }
+  ]
+});
+</script>
+<style scoped>
+.event_list {
+  overflow: hidden;
+  flex: 1;
+  
+}
+.comment {
+  position: absolute;
+  z-index: 10;
+  text-align: center;
+  width: 100%;
+  font-size: 25px;
+  color: #d51711;
+  padding: 20px;
+  box-sizing: border-box;
+}
+@media only screen and (max-width: 950px) {
+  .event_list {
+    padding-bottom: 12vw;
+  }
+  .comment {
+    padding: 3vw;
+    font-size: 5vw;
+  }
+}
+</style>
