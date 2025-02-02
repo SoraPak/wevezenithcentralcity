0298e8913e17f023a30391d3035a31194b2f3780
SoraPak <soragee86@gmail.com>
2025-02-02 15:19:31 +0900

    up

---
+++ pages/event_2410.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="event_list">
+    <Halloween />
+  </div>
+</template>
+
+<script setup>
+import { useHead } from '#imports';
+import Halloween from '~/components/landing/halloween.vue';
+
+useHead({
+  style: [
+    {
+      children: `
+        main {
+          display: flex !important;
+        }
+        .halloween {
+          margin-bottom: 60px !important;
+        }
+      
+        @media only screen and (max-width: 950px) {
+          .halloween {
+            margin-top: 0 !important;
+            min-height: 187vw !important;
+          }
+        
+        }
+      `
+    }
+  ]
+});
+</script>
+<style scoped>
+.event_list {
+  overflow: hidden;
+  flex: 1;
+  
+}
+</style>
