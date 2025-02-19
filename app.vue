1d63b8465ad591e632244d7d50b05a16bcdf7450
SoraPak <soragee86@gmail.com>
2025-02-19 13:51:52 +0900

    up add Google Tag Manager (noscript)

--- app.vue
+++ app.vue
@@ -1,26 +1,11 @@
 <template>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WN5JDZ92"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  </noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script setup>
-import { useHead } from '#imports';
-
-useHead({
-  script: [
-    {
-      innerHTML: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WN5JDZ92');
-      `,
-      type: 'text/javascript'
-    }
-  ],
-  bodyAttrs: {
-    class: 'gtm-enabled' // 필요 시 추가
-  }
-});
-</script>
